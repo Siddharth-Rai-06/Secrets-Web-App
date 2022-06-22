46668cbb999d8589089ca987c389a7c623551c56
Siddharth_Rai <sidrai0620@gmail.com>
2022-06-22 20:36:10 +0530

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint esversion:6
+require('dotenv').config();
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB",  {useNewUrlParser:true});
+//using ongoose.schema so that we can interfere with the format to encrypt it
+//it is not possible using simple JSON
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+//mongoose encrypts when we call save() and decrypts when we call find()
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
+//add plugin to the schema before creating mongoose model
+
+//creating databse model
+const User=mongoose.model("User",userSchema);
+
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  const newUser=new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login",function(req,res){
+  const username=req.body.username;
+  const password=req.body.password;
+  User.findOne({email:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password===password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+app.listen(3000,function(){
+  console.log("Server started listening on port 3000....");
+});
